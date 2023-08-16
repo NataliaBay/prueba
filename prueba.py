e1cf7d0f3c2f12ff649c32203dbf00f00e23b69f
bayonnat <nataliaandrea.bayonaperez@externals.adidas.com>
2023-08-16 14:56:49 -0500

    Se corrige error ciclo infinito

--- prueba.py
+++ prueba.py
@@ -10,21 +10,18 @@ while True:
                 2) Resta
                 3) División
                 4) Multiplicación
+                
+                opcion = int(input("Ingresa el número de la operación a realizar: "))  
 """)
-
-opcion = int(input("Ingresa el número de la operación a realizar: "))    
+  
  
 if opcion == 1:
     print("El resultado de la suma es de " ,n1,"+",n2," es igual a: ", n1+n2) 
-
 elif opcion == 2:
     print("El resultado de la resta es de " ,n1,"-",n2," es igual a: ", n1-n2) 
-
 elif opcion == 3:
     print("El resultado de la división es de " ,n1,"/",n2," es igual a: ",n1/n2)
-
-elif opcion == 1:
+elif opcion == 4:
     print("El resultado de la multiplicación es de " ,n1,"*",n2," es igual a: ", n1*n2) 
-
 else:
     print("Opción incorrecta")
