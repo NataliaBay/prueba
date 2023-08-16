06cc257c6329c5b00108843185467031f4d42744
bayonnat <nataliaandrea.bayonaperez@externals.adidas.com>
2023-08-16 14:36:46 -0500

    Se agrega elección de operaciones

---
+++ prueba.py
@@ -0,0 +1,13 @@
+n1 = int(input("Ingresa un  número: "))
+n2 = int(input("Ingresa un  número: "))
+
+opcion = 0
+while True: 
+    print("""
+            Elige la operación que deseas realizar:
+          
+                1) Suma
+                2) Resta
+                3) División
+                4) Multiplicación
+""")
