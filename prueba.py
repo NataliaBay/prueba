fa32260c5884eae93f8e4b59c4ef32a547f4ab51
bayonnat <nataliaandrea.bayonaperez@externals.adidas.com>
2023-08-16 14:46:55 -0500

    Se agregan las funciones para cada operación

--- prueba.py
+++ prueba.py
@@ -11,3 +11,20 @@ while True:
                 3) División
                 4) Multiplicación
 """)
+
+opcion = int(input("Ingresa el número de la operación a realizar: "))    
+ 
+if opcion == 1:
+    print("El resultado de la suma es de " ,n1,"+",n2," es igual a: " n1+n2) 
+
+elif opcion == 2:
+    print("El resultado de la resta es de " ,n1,"-",n2," es igual a: " n1-n2) 
+
+elif opcion == 3:
+    print("El resultado de la división es de " ,n1,"/",n2," es igual a: " n1/n2)
+
+elif opcion == 1:
+    print("El resultado de la multiplicación es de " ,n1,"*",n2," es igual a: " n1*n2) 
+
+else:
+    print("Opción incorrecta")
